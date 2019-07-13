643f73d989af3428b37723aafd2eeeeabe90ba38
Yi-Tse-Wu <44743933+tailer954@users.noreply.github.com>
2019-07-13 09:28:34 +0800

    Update Jolly Jumpers.c

--- One star problems/04、Math Calculation/Jolly Jumpers.c
+++ One star problems/04、Math Calculation/Jolly Jumpers.c
@@ -1,32 +1,46 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-#include <string.h>
-int coefficient[10000000];	//將係數定義為全域變數，是個聰明的方法，這樣微分函式和main函式都能使用
-
-int d(int x,int max)		//好像不一定要寫這個Fuction，這裡只是為了分開寫繪比較清晰吧
-{
-	long long sum=0,exp=1;
-	int i;
-	for(i=max-1;i>=0;i--)	//i初始化成max-1的原因：因Array最後一項是常數項，本項微分後必等於零，不必進行SUM的累加
-	{
-    	sum+=coefficient[i]*(max-i)*exp; //i=max-1時的coefficient[i]是X^1的係數，微分後相當於X^0=1。此時的max-i等於1，表微分後將係數乘1，
-    	exp = exp*x;		//exp表x的平方數
-    }
-	return sum;
-}
-
 int main(void)
 {
-    int x,i;
-    while(scanf("%d",&x)!=EOF)
+    int n,temp,set[3001],results[3001];
+    
+    while(scanf("%d",&n)!=EOF)
     {
-        for(i=0;;i++)
+        for(int i=0;i<n;i++)
+            scanf("%d",&set[i]);
+            
+        for(int i=0;i<n-1;i++)
         {
-        	scanf("%d",&coefficient[i]);	//本題題目要求方程式係數間須打空號，故利用scanf將每個係數值存入[i]中
-            if(getchar()=='\n')				//逐一讀取字元，讀到'\n'時斷開for loop
-                break;
+            temp = set[i+1]-set[i];
+            if(temp<0)
+                results[i]=-temp;
+            else
+                results[i]=temp;
         }
-    	printf("%d\n",d(x,i));				//套用Function解出應變數後將值輸出
+        
+        for(int i=0;i<n;i++) //bubble sort
+        {
+            for(int j=i+1;j<n-1;j++)
+            {
+                if(results[i]>results[j])
+                {
+                    temp=results[i];
+                    results[i]=results[j];
+                    results[j]=temp;
+                }
+            }
+        }
+        temp=0;
+        for(int i=0;i<n-1;i++) //confirm the gap==1
+        {
+            //printf("%d\n",results[i]);
+            if(results[i+1]-results[i]==1)
+                temp++;
+        }
+        if(temp==(n-2)&&results[0]==1)
+            printf("Jolly\n");
+        else
+            printf("Not jolly");
     }
     return 0;
 }
