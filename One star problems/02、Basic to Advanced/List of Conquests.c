af59757ba4adbe1bf8cee039bdb0655389aff723
Yi Tse Wu <44743933+tailer954@users.noreply.github.com>
2019-09-14 12:59:48 +0800

    Update List of Conquests.c

--- One star problems/02、Basic to Advanced/List of Conquests.c
+++ One star problems/02、Basic to Advanced/List of Conquests.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int cmpfunc (const void * a, const void * b) //qsort 需要的比較函式
+int cmpfunc (const void * a, const void * b) // The function which qsort need.
 {
     return (  *(int*)b - *(int*)a );
 }
 int main(void)
 {
     int n,i,j,k=0;
-    char temparray[76][2000],ans[76][2000];
-
     scanf("%d",&n);
-    char array[n][2000];
-    for(i=0;i<n;i++)
-    {
-        scanf("%s",array[i]);
-        fgets(temparray[i],2000,stdin);
-    }
-    qsort(array,sizeof(array)/sizeof(array[0]),sizeof(*array),cmpfunc);
+    char country[n][2000], name[76][2000];
 
     for(i=0;i<n;i++)
     {
-        int num =0,temp =0;
-        while(strcmp(array[i],array[i+temp]) == 0)  //don't write array[i]==array[i+temp]
-        {
-            num+=1;
-            temp+=1;
-        }
-        printf("%s %d\n",array[i],num);
-        i+=num-1;
+        scanf("%s",country[i]);
+        fgets(name[i],2000,stdin);
     }
 
+    qsort(country,n,2000,cmpfunc);  // (array name,array number,array size,compare function)
+
+    int num = 1;
+    for(i=0;i<n;i++)
+    {
+        if( strcmp(country[i],country[i+1])==0 )
+            num++;
+        else
+            printf("%s %d\n",country[i],num),num=1;
+    }
     return 0;
 }
